8598a9bd22b62194b0d26709d5bd81c50860186b
Drew J. Sonne <drew.sonne@gmail.com>
2021-09-15 13:46:08 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="piate",
-    version="0.1.4",
+    version="0.1.5",
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
     url="https://www.github.com/drewsonne/piate",
